c305af1cd6b9c8fdc5d22013642b3f13803e08e0
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-02-14 23:28:53 +0300

    dependency fixes and corrections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import EventBind from './components/EventBind'
-import Counter from './components/Counter'
-import Message from './components/Message'
-import Welcome from './components/Welcome'
-import Greet from './components/Greet'
+import EventBind from './components/EventBind';
+import Counter from './components/Counter';
+import Message from './components/Message';
+import Welcome from './components/Welcome';
+import Greet from './components/Greet';
 import ClickOutside from './components/ClickOutside';
 import StylingBasics from './components/StylingBasics';
 import FormHandling from './components/FormHandling';
